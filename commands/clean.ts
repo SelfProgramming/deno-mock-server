24eac0341f3c2dc2df2f6c5115251e96b438be3a
Dmytro Kyrii <dmytrokyrii@gmail.com>
2020-07-26 22:18:18 +0300

    add cache absents handler

--- commands/clean.ts
+++ commands/clean.ts
@@ -4,5 +4,12 @@ import { parse } from 'https://deno.land/std/flags/mod.ts';
 export function executeClean(): void {
   const { project = 'default' } = parse(Deno.args);
   const cacheDirPath = resolve('cache');
-  Deno.removeSync(`${cacheDirPath}/${project}-cache`, { recursive: true });
+  try {
+    Deno.removeSync(`${cacheDirPath}/${project}-cache`, { recursive: true });
+  } catch (e) {
+    console.error(`Project ${project} was not found (please, specify your project with --project= arg)`);
+    return;
+  }
+
+  console.log(`Cache for project ${project} has been cleaned`);
 }
